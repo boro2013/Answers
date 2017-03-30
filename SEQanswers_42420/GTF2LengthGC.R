8ca6a8eeaf0d1483f65962dfbda4ebc3f6e82dd9
DEBIT <adebit@doct.ulg.ac.be>
2017-03-30 10:47:50 +0200

    Update GTF2LengthGC.R

--- SEQanswers_42420/GTF2LengthGC.R
+++ SEQanswers_42420/GTF2LengthGC.R
@@ -7,10 +7,10 @@ GTFfile = "/home/ryand/Documents/Misc/Mus_musculus/Ensembl/GRCm38.71/Annotation/
 FASTAfile = "/home/ryand/Documents/Misc/Mus_musculus/Ensembl/GRCm38.71/Sequence/Mus_musculus.GRCm38.71.dna.toplevel.fa"
 
 #Load the annotation and reduce it
-GTF <- import.gff(GTFfile, format="gtf", genome="GRCm38.71", asRangedData=F, feature.type="exon")
+GTF <- import.gff(GTFfile, format="gtf", genome="GRCm38.71", feature.type="exon")
 grl <- reduce(split(GTF, elementMetadata(GTF)$gene_id))
 reducedGTF <- unlist(grl, use.names=T)
-elementMetadata(reducedGTF)$gene_id <- rep(names(grl), elementLengths(grl))
+elementMetadata(reducedGTF)$gene_id <- rep(names(grl), elementNROWS(grl))
 
 #Open the fasta file
 FASTA <- FaFile(FASTAfile)
